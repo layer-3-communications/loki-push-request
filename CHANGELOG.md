4c245db7b26c4ef73fadf3f76ea794e4ec604705
Andrew Martin <andrew.thaddeus@gmail.com>
2024-07-29 15:23:57 -0400

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for loki-push-request
+
+## 0.1.0.0 -- YYYY-mm-dd
+
+* First version. Released on an unsuspecting world.
---
+++ src/Loki/Push.hs
@@ -0,0 +1,81 @@
+{-# language DuplicateRecordFields #-}
+{-# language OverloadedStrings #-}
+{-# language PatternSynonyms #-}
+
+module Loki.Push
+  ( PushRequest(..)
+  , pushRequestToJson
+  ) where
+
+import Json (pattern (:->))
+import Data.Text (Text)
+import Data.Text.Short (ShortText)
+import Data.Int (Int64)
+import Data.Primitive (SmallArray,ByteArray(ByteArray))
+import Data.ByteString.Short.Internal (ShortByteString(SBS))
+
+import qualified Json
+import qualified Data.Primitive.Contiguous as Contiguous
+import qualified Data.Bytes.Builder.Bounded as Build
+import qualified Data.Text.Short.Unsafe as TSU
+import qualified Arithmetic.Nat as Nat
+
+newtype PushRequest = PushRequest { streams :: SmallArray Stream }
+
+data Stream = Stream
+  { labels :: !(SmallArray LabelPair)
+  , entries :: !(SmallArray Entry)
+  }
+
+data LabelPair = LabelPair
+  { name :: !ShortText
+  , value :: !ShortText
+  }
+
+data Entry = Entry
+  { timestamp :: !Int64
+  , line :: !ShortText
+  }
+
+-- Example JSON encoding from loki docs:
+--
+-- > {
+-- >   "streams": [
+-- >     {
+-- >       "stream": {
+-- >         "foo": "bar2"
+-- >       },
+-- >       "values": [
+-- >         [
+-- >           "1570818238000000000",
+-- >           "fizzbuzz"
+-- >         ]
+-- >       ]
+-- >     }
+-- >   ]
+-- > }
+--
+-- Strangely, this does not match what is suggested by the gogoproto
+-- files that the docs link to.
+
+
+pushRequestToJson :: PushRequest -> Json.Value
+pushRequestToJson (PushRequest streams) = Json.object1
+  ("streams" :-> Json.Array (fmap streamToJson streams)
+  )
+  
+streamToJson :: Stream -> Json.Value
+streamToJson Stream{labels,entries} = Json.object2
+  ("stream" :-> Json.Object (fmap labelPairToMember labels))
+  ("values" :-> Json.Array (fmap entryToJson entries))
+
+entryToJson :: Entry -> Json.Value
+entryToJson Entry{timestamp,line} = Json.Array $ Contiguous.doubleton
+  ( case Build.run Nat.constant (Build.int64Dec timestamp) of
+      ByteArray x -> Json.String (TSU.fromShortByteStringUnsafe (SBS x))
+  )
+  (Json.String line)
+
+labelPairToMember :: LabelPair -> Json.Member
+labelPairToMember LabelPair{name,value} =
+  Json.Member{key = name, value = Json.String value}
